9c17f5f7d06bdeb951a6751fba062ded87aa374a
Michaelr12 <35987510+Michaelr12@users.noreply.github.com>
2018-04-10 17:23:03 -0700

    Add files via upload

---
+++ jensen.js
@@ -0,0 +1,42 @@
+
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+//background color match
+
+var background = [
+rgb(00,255,255);
+rgb(255,00,255);
+rgb(00,00,255);
+rgb(00,255,00);
+rgb(255,255,00);
+]
+
+for(var i = 0; i.arr.length; i++)
